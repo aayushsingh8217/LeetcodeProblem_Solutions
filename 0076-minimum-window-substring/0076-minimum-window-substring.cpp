c1010fe688e6511cca7ab273e6411e3f0cc950ab
aayushsingh8217 <98265835+aayushsingh8217@users.noreply.github.com>
2023-05-20 21:10:33 +0530

    Time: 32 ms (41.29%), Space: 8 MB (65.68%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+//Facebook,Amazon,LinkedIN,Lyft,Microsoft,Airbnb,Apple,Adobe,Snapchat,Oracle
+    string minWindow(string s, string t) {
+        unordered_map<char, int> mp;
+        string res = "";
+        int size = INT_MAX;
+        int i = 0, j = 0;
+        int st, ed;
+        for (auto it : t) {
+            mp[it]++;
+        }
+        int count = mp.size();
+        while (j < s.size() && i < s.size()) {
+            if (mp.count(s[j]) > 0) {
+                mp[s[j]]--;
+                if (mp[s[j]] == 0)
+                    count--;
+            }
+            j++;
+            while (count == 0) {
+                if (j - i < size) {
+                    size = j - i;
+                    st = i;
+                    ed = j;
+                }
+                if (mp.count(s[i]) > 0) {
+                    mp[s[i]]++;
+                    if (mp[s[i]] == 1)
+                        count++;
+                }
+                i++;
+            }
+        }
+        for (int k = st; k < ed; k++)
+            res += s[k];
+        return res;
+    }
+};
