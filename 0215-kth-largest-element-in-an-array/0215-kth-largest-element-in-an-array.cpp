e7d81c22be54f2ab7ff7b68894e3b363c1daa76a
aayushsingh8217 <98265835+aayushsingh8217@users.noreply.github.com>
2023-05-23 01:02:25 +0530

    Time: 137 ms (35.00%), Space: 47.3 MB (46.14%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    //kth largest element 
+    //sorting kar skte hai lekin usse phr nlogn ka TC aayega
+    //Heap use kr sakte hai saari conditions bhi satisfy kar rahi hai
+    //largest toh min heap
+    int findKthLargest(vector<int>& nums, int k) {
+       priority_queue <int,vector<int>,greater<int>> minh;
+        for(int i=0;i<nums.size();i++)
+        {
+            minh.push(nums[i]);
+            if(minh.size()>k)
+                minh.pop();
+        }
+        return minh.top();
+    }
+};
