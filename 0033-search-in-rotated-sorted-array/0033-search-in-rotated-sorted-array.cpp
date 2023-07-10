9a13a610c782585d6476137c62dc446f89adcfd9
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2023-07-10 15:27:29 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (47.97%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,56 @@
 class Solution {
 public:
-//Amazon,Faebook,Microsoft,LinkedIN,apple,Bloomberg,adobe,bytedance,walmart,oracle,tiktok,vmware,google,visa,servicenow,expedja,nvidia,salesforce,Goldman sachs,morgan stanley,splunk
     int search(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-        while(l<=r)
-        {
-            int m=(l+r)/2;
-            if(nums[m]==target)
-            return m;
-
-            if(nums[l]<=nums[m])
-            {
-                if(nums[l]<=target && target<nums[m])
-                r=m-1;
-                else 
-                l=m +1;
-            }
+  int n = nums.size();
+        int idx = findMin(nums);
+        int BS1 = binarySearch(nums ,  0, idx - 1,target);
+        int BS2 = binarySearch(nums , idx, n-1,target);
+        if(BS1 == -1)
+            return BS2;
+        else
+            return BS1;
+    }
+    
+    int binarySearch(vector<int>& nums, int left, int right, int target) {
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            
+            if (nums[middle] == target)
+                return middle;
+            else if (target > nums[middle])
+                left = middle + 1;
             else
+                right = middle - 1;
+        }
+        
+        return -1;
+    }
+     int findMin(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int start = 0, end = n-1;
+        if(nums[start] < nums[end])
+        {
+            return 0;
+        }
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            int next = (mid+1)%n;
+            int prev = (mid + n - 1)%n;
+            if(nums[mid] <= nums[next] && nums[mid] <= nums[prev])
             {
-                 if (nums[m] < target && target <= nums[r]) 
-                l=m+1;
-            else 
-                r=m-1;
+                return mid;
+            }
+            if(nums[mid] <= nums[end])
+            {
+                end = mid - 1;
+            }
+            else if(nums[start] <= nums[mid])
+            {
+                start = mid + 1;
             }
         }
-    return -1;
+        return -1;
     }
 };
