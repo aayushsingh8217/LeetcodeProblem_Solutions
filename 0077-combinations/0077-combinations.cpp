3b4890be7c94eb8f143307886db81b7eab8e4177
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2023-08-01 09:19:49 +0530

    Time: 21 ms (85.40%), Space: 9.1 MB (69.03%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> combine(int n, int k) {
-          vector<vector<int>> result;
-    vector<int> current_combination;
-    backtrack(n, k, 1, current_combination, result);
-    return result;
+       vector <vector <int>> res;
+        vector <int> curr_comb;
+        backtrack(n,k,1,curr_comb,res);
+        return res;
     }
-    void backtrack(int n, int k, int start, vector<int>& current_combination, vector<vector<int>>& result) {
-    if (current_combination.size() == k) {
-        result.push_back(current_combination);
-        return;
-    }
-
-    for (int i = start; i <= n; ++i) {
-        current_combination.push_back(i);
-        backtrack(n, k, i+1 , current_combination, result);
-        current_combination.pop_back();
-    }
-}
+  void backtrack(int n,int k,int idx,vector <int> & curr_comb,vector <vector<int>>& res)
+  {
+      if(curr_comb.size()==k)
+      {
+          res.push_back(curr_comb);
+          return;
+      }
+      for(int i=idx;i<=n;i++)
+      {
+          curr_comb.push_back(i);
+          backtrack(n,k,i+1,curr_comb,res);
+          curr_comb.pop_back();
+      }
+  }
 };
