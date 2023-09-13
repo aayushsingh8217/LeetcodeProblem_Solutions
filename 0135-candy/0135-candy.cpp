2c15505a71372f34052bf928550ac456de26d83a
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2023-09-13 23:20:25 +0530

    Time: 19 ms (45.37%), Space: 18 MB (29.28%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    if (n <= 1) {
+        return n;
+    }
+
+    vector<int> candies(n, 1);
+
+    for (int i = 1; i < n; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = max(candies[i], candies[i + 1] + 1);
+        }
+    }
+
+    int totalCandies = 0;
+    for (int i = 0; i < n; i++) {
+        totalCandies += candies[i];
+    }
+
+    return totalCandies;      
+    }
+};
