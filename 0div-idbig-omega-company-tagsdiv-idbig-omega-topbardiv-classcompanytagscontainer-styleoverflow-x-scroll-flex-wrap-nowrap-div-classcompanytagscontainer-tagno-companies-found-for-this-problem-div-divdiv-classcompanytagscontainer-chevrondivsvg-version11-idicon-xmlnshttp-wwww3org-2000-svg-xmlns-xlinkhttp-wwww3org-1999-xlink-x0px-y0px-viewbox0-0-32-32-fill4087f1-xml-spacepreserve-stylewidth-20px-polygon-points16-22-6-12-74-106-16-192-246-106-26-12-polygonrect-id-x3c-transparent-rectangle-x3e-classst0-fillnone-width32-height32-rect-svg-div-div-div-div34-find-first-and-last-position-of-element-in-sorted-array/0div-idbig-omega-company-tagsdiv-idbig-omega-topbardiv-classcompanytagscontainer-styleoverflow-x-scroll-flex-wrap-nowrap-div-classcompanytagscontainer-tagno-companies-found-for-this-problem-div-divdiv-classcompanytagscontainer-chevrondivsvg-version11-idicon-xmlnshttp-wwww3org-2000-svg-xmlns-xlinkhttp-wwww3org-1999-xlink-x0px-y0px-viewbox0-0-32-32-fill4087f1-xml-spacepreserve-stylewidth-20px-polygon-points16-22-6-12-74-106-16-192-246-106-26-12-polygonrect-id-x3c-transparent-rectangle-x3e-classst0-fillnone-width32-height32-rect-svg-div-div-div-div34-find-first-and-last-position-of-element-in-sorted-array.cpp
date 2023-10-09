d2e091dc913b8e51559e17fca2d128d9ce6fe5b3
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2023-10-10 03:38:48 +0530

    Time: 7 ms (51.19%), Space: 14 MB (26.25%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start=0,end=nums.size()-1;
+        // vector <int> res(2,-1); isse kya hoga jab first and last occurnce found hogi toh vo thirst and fourth position par push ho jaegi
+        vector <int> res;
+        int fo=-1,lo=-1;
+        if(nums.size()==1 && nums[0]==target)
+        {
+            res.push_back(0);
+            res.push_back(0);
+            return res;
+        }
+        else if(nums.size()==1 && nums[0]!=target)
+        {
+            res.push_back(fo);
+            res.push_back(lo);
+            return res;
+        }
+        else
+        {
+            //First occurence
+            while(start<=end)
+            {
+                int mid=start+(end-start)/2;
+                if(nums[mid]==target)
+                {
+                    fo=mid;
+                    end=mid-1;
+                }
+                else if(target>nums[mid])
+                    start=mid+1;
+                else
+                    end= mid-1;
+            }
+            res.push_back(fo);
+            //Last occurence
+            start=0;
+            end=nums.size()-1;
+            while(start<=end)
+            {
+                int mid=start+(end-start)/2;
+                if(nums[mid]==target)
+                {
+                    lo=mid;
+                    start=mid+1;
+                }
+                else if(target>nums[mid])
+                    start=mid+1;
+                else
+                    end=mid-1;
+            }
+            res.push_back(lo);
+            
+        }
+        return res;
+    }
+};
