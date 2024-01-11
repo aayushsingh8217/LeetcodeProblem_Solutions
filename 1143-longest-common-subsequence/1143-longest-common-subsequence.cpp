e00c66a86474ccbc6d96af4679c8c2e01400e6f8
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-01-11 06:28:51 +0530

    Time: 43 ms (71.74%), Space: 24.7 MB (67.48%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -19,25 +19,52 @@ public:
 //         return dp[i][j]=1+max_sub(a,b,i+1,j+1);
 //         return dp[i][j]=max(max_sub(a,b,i,j+1),max_sub(a,b,i+1,j));
 //     }
-    int dp[1001][1001];
+    
+    
+    //MEMOIZATION
+//     int dp[1001][1001];
+//          int longestCommonSubsequence(string text1, string text2) {
+//           int n=text1.size();
+//              int m=text2.size();
+//              memset(dp,-1,sizeof(dp));
+//              return solverecur(n,m,text1,text2);
+             
+        
+//      }
+//     int solverecur(int n,int m,string &text1,string &text2)
+//     {
+//         if(n==0 || m==0)
+//             return 0;
+//         if(dp[n][m]!=-1)
+//             return dp[n][m];
+//         if(text1[n-1]==text2[m-1])
+//             return dp[n][m]=1+solverecur(n-1,m-1,text1,text2);
+//         else
+//             return dp[n][m]=max(solverecur(n-1,m,text1,text2),solverecur(n,m-1,text1,text2));
+        
+//     }
+    
+    
          int longestCommonSubsequence(string text1, string text2) {
           int n=text1.size();
              int m=text2.size();
-             memset(dp,-1,sizeof(dp));
-             return solverecur(n,m,text1,text2);
+             
+             vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+             
+             for(int i=1;i<=n;i++)
+             {
+                 for(int j=1;j<=m;j++)
+                 {
+                     if(text1[i-1]==text2[j-1])
+                         dp[i][j]=dp[i-1][j-1]+1;
+                     else
+                         dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                 }
+             }
+             
+             return dp[n][m];
              
         
      }
-    int solverecur(int n,int m,string &text1,string &text2)
-    {
-        if(n==0 || m==0)
-            return 0;
-        if(dp[n][m]!=-1)
-            return dp[n][m];
-        if(text1[n-1]==text2[m-1])
-            return dp[n][m]=1+solverecur(n-1,m-1,text1,text2);
-        else
-            return dp[n][m]=max(solverecur(n-1,m,text1,text2),solverecur(n,m-1,text1,text2));
-        
-    }
+    
 };
