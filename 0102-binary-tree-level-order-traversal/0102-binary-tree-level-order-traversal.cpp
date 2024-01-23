96fe549f81fa82593ca75b3a08645b2af098ecbf
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-01-23 18:30:59 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (5.39%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,28 +11,47 @@
  */
 class Solution {
 public:
+    vector<vector<int>> res;
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> res;
-        if(root==NULL)
-            return res;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty())
-        {
-            vector<int> singleLvl;
-            int n=q.size();
-            while(n--)
-            {
-                TreeNode* node=q.front();
-                q.pop();
-                singleLvl.push_back(node->val);
-                if(node->left)
-                    q.push(node->left);
-                if(node->right)
-                    q.push(node->right);
-            }
-            res.push_back(singleLvl);
-        }
+        
+        //----------------BFS--------
+        // vector<vector<int>> res;
+        // if(root==NULL)
+        //     return res;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty())
+        // {
+        //     vector<int> singleLvl;
+        //     int n=q.size();
+        //     while(n--)
+        //     {
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         singleLvl.push_back(node->val);
+        //         if(node->left)
+        //             q.push(node->left);
+        //         if(node->right)
+        //             q.push(node->right);
+        //     }
+        //     res.push_back(singleLvl);
+        // }
+        // return res;
+        
+        //-------------------DFS-----------------
+        solve(root,0);
         return res;
+    }
+    void solve(TreeNode* root,int dep)
+    {
+        if(root==NULL)
+            return ;
+        
+        if(res.size()==dep)
+            res.push_back(vector<int>());
+        
+        res[dep].push_back(root->val);
+        solve(root->left,dep+1);
+        solve(root->right,dep+1);
     }
 };
