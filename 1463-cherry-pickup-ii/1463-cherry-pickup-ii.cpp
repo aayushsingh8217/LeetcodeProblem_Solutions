e1bbcbeee67e49515ac4beadd6955a3f47ae253b
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-02-11 14:11:55 +0530

    Time: 68 ms (66.57%), Space: 19.5 MB (11.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    int cherryPickup(vector<vector<int>>& grid) {
-       int rows = grid.size();
-    int cols = grid[0].size();
-    
-    vector<vector<vector<int>>> dp(rows, vector<vector<int>>(cols, vector<int>(cols, 0)));
-    
-    for (int i = rows - 1; i >= 0; --i) {
-        for (int j = 0; j < cols; ++j) {
-            for (int k = 0; k < cols; ++k) {
-                int currentCherries = 0;
-                currentCherries += grid[i][j];
-                if (j != k) {
-                    currentCherries += grid[i][k];
-                }
-                if (i != rows - 1) {
-                    int maxCherries = 0;
-                    for (int dj = -1; dj <= 1; ++dj) {
-                        for (int dk = -1; dk <= 1; ++dk) {
-                            int nj = j + dj;
-                            int nk = k + dk;
-                            if (nj >= 0 && nj < cols && nk >= 0 && nk < cols) {
-                                maxCherries = max(maxCherries, dp[i + 1][nj][nk]);
-                            }
-                        }
-                    }
-                    currentCherries += maxCherries;
-                }
-                dp[i][j][k] = currentCherries;
-            }
-        }
+     bool isValid(int i, int j, int rows, int cols) {
+        return i >= 0 && i < rows && j >= 0 && j < cols;
     }
+    int cherryPickup(vector<vector<int>>& grid) {
+ int rows = grid.size();
+       int cols = grid[0].size();
     
-    return dp[0][0][cols - 1];  
+       vector<vector<vector<int>>> dp(rows, vector<vector<int>>(cols, vector<int>(cols, 0)));
+    
+       for (int i = rows - 1; i >= 0; --i) {
+           for (int j = 0; j < cols; ++j) {
+               for (int k = 0; k < cols; ++k) {
+                   int currentCherries = 0;
+                   currentCherries += grid[i][j];
+                   if (j != k) {
+                       currentCherries += grid[i][k];
+                   }
+                   if (i != rows - 1) {
+                       int maxCherries = 0;
+                       for (int dj = -1; dj <= 1; ++dj) {
+                           for (int dk = -1; dk <= 1; ++dk) {
+                               int nj = j + dj;
+                               int nk = k + dk;
+                               if (isValid(i + 1, nj, rows, cols) && isValid(i + 1, nk, rows, cols)) {
+                                   maxCherries = max(maxCherries, dp[i + 1][nj][nk]);
+                               }
+                           }
+                       }
+                       currentCherries += maxCherries;
+                   }
+                   dp[i][j][k] = currentCherries;
+               }
+           }
+       }
+    
+       return dp[0][0][cols - 1]; 
     }
 };
