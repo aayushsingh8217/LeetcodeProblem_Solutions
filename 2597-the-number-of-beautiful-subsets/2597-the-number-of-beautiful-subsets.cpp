9b84b4a5c1b0d5c9bb5d21ea82c0d3a583e08fbf
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-05-23 20:54:44 +0530

    Time: 792 ms (14.26%), Space: 38.5 MB (44.42%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countBeautifulSubsets;
+    int difference;
+
+    void findBeautifulSubsets(vector<int>& nums, int index, unordered_map<int, int>& elementCount) {
+        if (index == nums.size()) {
+            countBeautifulSubsets++;
+            return;
+        }
+
+        findBeautifulSubsets(nums, index + 1, elementCount);
+
+        if (!elementCount[nums[index] - difference] && !elementCount[nums[index] + difference]) {
+            elementCount[nums[index]]++;
+            findBeautifulSubsets(nums, index + 1, elementCount);
+            elementCount[nums[index]]--;  // Backtrack
+        }
+    }
+    int beautifulSubsets(vector<int>& nums, int k) {
+        countBeautifulSubsets = 0;
+        difference = k;
+        unordered_map<int, int> elementCount;
+
+        findBeautifulSubsets(nums, 0, elementCount);
+
+        return countBeautifulSubsets - 1; 
+    }
+};
