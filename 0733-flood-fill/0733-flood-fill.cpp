20356166bae961f76bcf94e1196efe8fd430cb72
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-09-29 16:08:57 +0530

    Time: 0 ms (100%), Space: 17.2 MB (50.72%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,58 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m=image.size();
-        int n=image[0].size();
+        // int m=image.size();
+        // int n=image[0].size();
         //BFS
+        // int currc=image[sr][sc];
+        // queue <pair<int,int>> q;
+        // q.push({sr,sc});
+        // set <pair<int,int>> vis;
+        
+        // vector<vector<int>> neigh={{1,0},{0,1},{0,-1},{-1,0}};
+        // while(!q.empty())
+        // {
+        //     auto [x,y]=q.front();
+        //     q.pop();
+        //     image[x][y]=color;
+        //     vis.insert({x,y});
+        //     for(auto &it:neigh)
+        //     {
+        //         int dx=it[0],dy=it[1];
+        //         int nx=x+dx,ny=y+dy;
+        //         if(nx>=0 && ny>=0 && nx<m && ny<n && image[nx][ny]==currc && vis.find({nx,ny})==vis.end() )
+        //             q.push({nx,ny});
+        //     }
+
+        // }
+        // return image;
+
+
+
+        // DFS
         int currc=image[sr][sc];
-        queue <pair<int,int>> q;
-        q.push({sr,sc});
-        set <pair<int,int>> vis;
-        //vis.insert({sr,sc});
         vector<vector<int>> neigh={{1,0},{0,1},{0,-1},{-1,0}};
-        while(!q.empty())
-        {
-            auto [x,y]=q.front();
-            q.pop();
-            image[x][y]=color;
-            vis.insert({x,y});
+        set <pair<int,int>> vis;
+        solvedfs(image,sr,sc,currc,color,neigh,vis);
+        return image;
+
+    }
+    void solvedfs(vector<vector<int>>& image,int x,int y,int currc,int color,vector<vector<int>>& neigh,set <pair<int,int>>& vis)
+    {
+        int m=image.size();
+         int n=image[0].size();
+       image[x][y]=color;
+       
+       vis.insert({x,y});
+       
             for(auto &it:neigh)
             {
                 int dx=it[0],dy=it[1];
                 int nx=x+dx,ny=y+dy;
                 if(nx>=0 && ny>=0 && nx<m && ny<n && image[nx][ny]==currc && vis.find({nx,ny})==vis.end() )
-                    q.push({nx,ny});
-
-
-                
-            }
-
-        }
-        return image;
+                    solvedfs(image,nx,ny,currc,color,neigh,vis);
+             }
 
     }
+    
 };
