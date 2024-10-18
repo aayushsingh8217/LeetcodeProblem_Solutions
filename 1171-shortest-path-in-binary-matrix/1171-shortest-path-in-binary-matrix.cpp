67b1ac6cab47472529737b2cf25c7e708f3c3cc3
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-10-18 14:42:05 +0530

    Time: 282 ms (5.39%), Space: 83.1 MB (5.29%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -15,46 +15,86 @@ vector<vector<int>> direc{{1,1},{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}}
             return false;
         };
 
-        queue <pair<int,int>> q;
+        
 
         if(m==0 || n==0 || grid[0][0]!=0)
         return -1;
+        // BFS---------------------------------------
+        // queue <pair<int,int>> q;
+        // q.push({0,0});
+        // grid[0][0]=1;
 
-        q.push({0,0});
-        grid[0][0]=1;
+        // int level=0;
 
-        int level=0;
+        // while(!q.empty())
+        // {
+        //     int l=q.size();
 
-        while(!q.empty())
-        {
-            int l=q.size();
+        //     while(l--)
+        //     {
+        //         auto curr=q.front();
+        //         q.pop();
 
-            while(l--)
-            {
-                auto curr=q.front();
-                q.pop();
+        //         int x=curr.first;
+        //         int y=curr.second;
 
-                int x=curr.first;
-                int y=curr.second;
-
-                if(x==m-1 && y==n-1)
-                return level+1;
+        //         if(x==m-1 && y==n-1)
+        //         return level+1;
                 
 
-                for(auto dir:direc)
-                {
-                    int x_=x+dir[0];
-                    int y_=y+dir[1];
+        //         for(auto dir:direc)
+        //         {
+        //             int x_=x+dir[0];
+        //             int y_=y+dir[1];
 
-                    if(isValid(x_,y_) && grid[x_][y_]==0)
-                    {
-                        q.push({x_,y_});
-                        grid[x_][y_]=1;
-                    }
+        //             if(isValid(x_,y_) && grid[x_][y_]==0)
+        //             {
+        //                 q.push({x_,y_});
+        //                 grid[x_][y_]=1;
+        //             }
+        //         }
+        //     }
+        //     level++;
+        // }
+        // return -1;
+
+
+
+        // DJ__________________________________________________________
+
+        vector<vector<int>> res(m,vector<int>(n,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        pq.push({0,{0,0}});
+        res[0][0]=0;
+
+
+        while(!pq.empty())
+        {
+            int d=pq.top().first;
+            auto node=pq.top().second;
+            int x=node.first;
+            int y=node.second;
+
+            pq.pop();
+
+            for(auto dir:direc)
+            {
+                int x_=x+dir[0];
+                int y_=y+dir[1];
+
+                int dist=1;
+
+                if(isValid(x_,y_) && grid[x_][y_]==0 && d+dist<res[x_][y_])
+                {
+                    pq.push({d+dist,{x_,y_}});
+                    res[x_][y_]=d+dist;
+
                 }
             }
-            level++;
         }
+        if(res[m-1][n-1]==INT_MAX)
         return -1;
+        else return 1+res[m-1][n-1];
+
     }
 };
