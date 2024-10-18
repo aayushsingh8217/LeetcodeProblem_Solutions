6eb130ea315ba958b4efbf54563984686add1b84
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-10-18 16:01:20 +0530

    Time: 265 ms (8.26%), Space: 69.7 MB (6.9%) - LeetHub

---
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+vector<vector<int>> direc ={{0,1},{1,0},{-1,0},{0,-1}};
+    int minimumEffortPath(vector<vector<int>>& heights) {
+      int m=heights.size();
+      int n=heights[0].size();
+
+      vector<vector<int>> res(m,vector<int>(n,INT_MAX));
+      priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+      pq.push({0,{0,0}});
+      res[0][0]=0;
+
+      auto isValid=[&](int x,int y)
+      {
+        if(x>=0 && y>=0 && x<m && y<n)
+        return true;
+        return false;
+      };
+
+      while(!pq.empty())
+      {
+        int d=pq.top().first;
+        auto node=pq.top().second;
+        pq.pop();
+        int x=node.first;
+        int y=node.second;
+
+        for(auto dir:direc)
+        {
+            int x_=x+dir[0];
+            int y_=y+dir[1];
+
+            if(isValid(x_,y_))
+            {
+                int absdiff=abs(heights[x][y]-heights[x_][y_]);
+                int maxdiff=max(d,absdiff);
+                if(res[x_][y_]>maxdiff)
+                {
+                    res[x_][y_]=maxdiff;
+                    pq.push({maxdiff,{x_,y_}});
+                }
+            }
+        }
+      }
+      return res[m-1][n-1]; 
+    }
+};
