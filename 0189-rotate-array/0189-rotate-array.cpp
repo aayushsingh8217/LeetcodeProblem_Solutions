bfb746ee934dc61567122ac8d62879c222ee6727
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-10-21 19:14:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-       //-------------Counter clockwise rotation-----------------
-       
-        // k=k%nums.size();
-
-        // reverse(nums.begin(),nums.begin()+k);
-        // reverse(nums.begin()+k,nums.end());
-
-        // reverse(nums.begin(),nums.end());
-
-
-        //---------------clockwise rotation--------------------
-
-        int n = nums.size();
-k = k % n;  
-int idxfromstart = n - k;
-reverse(nums.begin(), nums.begin() + idxfromstart);
-reverse(nums.begin() + idxfromstart, nums.end());
-reverse(nums.begin(), nums.end());
+        if(nums.size()<2)
+        return;
+       reverse(nums.begin(),nums.end());
+       reverse(nums.begin()+k,nums.end()); 
+       reverse(nums.begin(),nums.begin()+k);
     }
 };
