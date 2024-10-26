882685bcf1d0d9bfe2494a4b227d5746242d9a42
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-10-27 01:36:55 +0530

    Time: 0 ms (100%), Space: 8.8 MB (83.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-         int n = matrix.size();
-    vector < vector < int >> rotated(n, vector < int > (n, 0));
-    for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        rotated[j][n - i - 1] = matrix[i][j];
-    }
-    }
+    //      int n = matrix.size();
+    // vector < vector < int >> rotated(n, vector < int > (n, 0));
+    // for (int i = 0; i < n; i++) {
+    // for (int j = 0; j < n; j++) {
+    //     rotated[j][n - i - 1] = matrix[i][j];
+    // }
+    // }
+    // for(int i=0;i<n;i++)
+    // {
+    //     for(int j=0;j<n;j++)
+    //     {
+    //         matrix[i][j]=rotated[i][j];
+    //     }
+    // }
+
+    int n=matrix.size();
+
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<i;j++)
         {
-            matrix[i][j]=rotated[i][j];
+            swap(matrix[i][j],matrix[j][i]);
         }
     }
+
+    for(int i=0;i<n;i++)
+    {
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+
     }
 };
