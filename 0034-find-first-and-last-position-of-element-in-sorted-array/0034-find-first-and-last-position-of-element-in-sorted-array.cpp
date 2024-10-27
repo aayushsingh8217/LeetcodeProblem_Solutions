741e829db80ef0b5628f63494d0fd43514d6231f
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2024-10-27 15:23:16 +0530

    Time: 0 ms (100%), Space: 16.3 MB (39.75%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,58 +1,46 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start=0,end=nums.size()-1;
-        // vector <int> res(2,-1); isse kya hoga jab first and last occurnce found hogi toh vo thirst and fourth position par push ho jaegi
-        vector <int> res;
         int fo=-1,lo=-1;
-        if(nums.size()==1 && nums[0]==target)
+
+        int low =0,high=nums.size()-1;
+        vector<int> res(2,-1);
+        //fo
+        while(low<=high)
         {
-            res.push_back(0);
-            res.push_back(0);
-            return res;
-        }
-        else if(nums.size()==1 && nums[0]!=target)
-        {
-            res.push_back(fo);
-            res.push_back(lo);
-            return res;
-        }
-        else
-        {
-            //First occurence
-            while(start<=end)
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target)
             {
-                int mid=start+(end-start)/2;
-                if(nums[mid]==target)
-                {
-                    fo=mid;
-                    end=mid-1;
-                }
-                else if(target>nums[mid])
-                    start=mid+1;
-                else
-                    end= mid-1;
+                fo=mid;
+                high=mid-1;
             }
-            res.push_back(fo);
-            //Last occurence
-            start=0;
-            end=nums.size()-1;
-            while(start<=end)
-            {
-                int mid=start+(end-start)/2;
-                if(nums[mid]==target)
-                {
-                    lo=mid;
-                    start=mid+1;
-                }
-                else if(target>nums[mid])
-                    start=mid+1;
-                else
-                    end=mid-1;
-            }
-            res.push_back(lo);
-            
+            else if(nums[mid]<target)
+            low=mid+1;
+            else
+            high=mid-1;
         }
+
+        //lo
+        low=0,high=nums.size()-1;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target)
+            {
+                lo=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target)
+            high=mid-1;
+            else
+            low=mid+1;
+        }
+
+        res[0]=fo;
+        res[1]=lo;
+
         return res;
     }
 };
