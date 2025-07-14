f104771fd1f47106298360a97abe6653c9fddba3
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2025-07-14 15:03:11 +0530

    Time: 3726 ms (5%), Space: 0B (100%) - LeetHub

--- 1827-invalid-tweets/1827-invalid-tweets.sql
+++ 1827-invalid-tweets/1827-invalid-tweets.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
 #strictly greater than 15 hona chahyea
+-- select tweet_id
+-- from Tweets
+-- where length(content)>15 #varchar type ka hai
+
 select tweet_id
 from Tweets
-where length(content)>15 #varchar type ka hai
+where length(content)>15
