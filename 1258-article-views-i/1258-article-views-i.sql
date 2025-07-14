e6d00873a271f32885d424a096c180a69b6bde53
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2025-07-14 14:58:14 +0530

    Time: 3609 ms (5%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-select distinct #duplicates hatane ke liye
-author_id as id  #alias as id result mein id hai
-from Views
-where author_id=viewer_id  #table mein duplicates bhi hai
+-- select distinct #duplicates hatane ke liye
+-- author_id as id  #alias as id result mein id hai
+-- from Views
+-- where author_id=viewer_id  #table mein duplicates bhi hai
+-- order by author_id
+
+select distinct
+author_id as id from Views
+where author_id = viewer_id
 order by author_id
