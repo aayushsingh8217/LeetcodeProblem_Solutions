68267e733d380389cc4b315f113c14bd8bb0c837
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2025-07-14 18:06:07 +0530

    Time: 1528 ms (9.91%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
+-- select eu.unique_id, e.name
+-- from Employees as e
+-- #foreign key concept
+-- left join EmployeeUNI as eu
+-- on e.id=eu.id
+
+
 select eu.unique_id, e.name
-from Employees as e
-#foreign key concept
-left join EmployeeUNI as eu
+from Employees as e left join EmployeeUNI as eu
 on e.id=eu.id
