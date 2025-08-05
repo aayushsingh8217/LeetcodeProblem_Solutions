106e4431cff41d841d55398d9239bc938cb2ca5b
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2025-08-05 17:05:57 +0530

    Time: 0 ms (100%), Space: 17.7 MB (17.22%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,46 +1,59 @@
 class Solution {
 public:
+int bsfo(vector<int> & nums,int target)
+{
+    int l =0, h = nums.size()-1;
+    int f=-1;
+    while(l<=h)
+    {
+        int m = l+(h-l)/2;
+        if(nums[m] == target )
+        {
+            f = m;
+            h = m-1;
+        }
+        else if(nums[m]> target)
+        {
+            h = m-1;
+        }
+        else
+        l = m+1;
+    }
+    return f;
+}
+int bslo(vector<int> & nums, int target)
+{
+    int l =0, h = nums.size()-1;
+    int lo=-1;
+    while(l<=h)
+    {
+        int m = l+(h-l)/2;
+        if(nums[m]== target)
+        {
+            lo = m;
+            l = m+1;
+        }
+        else if(nums[m]> target)
+        {
+            h = m-1;
+        }
+        else
+        l = m+1;
+    }
+    return lo;
+}
     vector<int> searchRange(vector<int>& nums, int target) {
-        int fo=-1,lo=-1;
-
-        int low =0,high=nums.size()-1;
-        vector<int> res(2,-1);
-        //fo
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
-            {
-                fo=mid;
-                high=mid-1;
-            }
-            else if(nums[mid]<target)
-            low=mid+1;
-            else
-            high=mid-1;
-        }
-
-        //lo
-        low=0,high=nums.size()-1;
-
-        while(low<=high)
-        {
-            int mid=low+(high-low)/2;
-
-            if(nums[mid]==target)
-            {
-                lo=mid;
-                low=mid+1;
-            }
-            else if(nums[mid]>target)
-            high=mid-1;
-            else
-            low=mid+1;
-        }
-
-        res[0]=fo;
-        res[1]=lo;
-
-        return res;
+        vector<int> range;
+        int fo = bsfo(nums,target);
+        int lo = bslo(nums,target);
+        if(fo!=-1)
+        range.push_back(fo);
+        else 
+        range.push_back(-1);
+        if(lo!=-1)
+        range.push_back(lo);
+        else
+        range.push_back(-1);
+        return range;
     }
 };
