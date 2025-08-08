25c4f6d2d349d5dfb276a1379f94f0504d6984fd
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2025-08-08 08:52:18 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -7,14 +7,13 @@ public:
         while(i>=0 && i<n && j>=0 && j<m)
         {
             if(matrix[i][j]==target)
-            {
-                return true;
-            }
-            else if(matrix[i][j]<target)
-                i++;
-            else
-                j--;
+            return true;
+            else if(matrix[i][j]>target)
+            j--;
+            else 
+            i++;
         }
         return false;
     }
+
 };
