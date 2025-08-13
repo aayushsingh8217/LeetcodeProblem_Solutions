338fee3a566d9b34d7921fde040c72445e0ca0ef
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2025-08-13 18:03:13 +0530

    Time: 159 ms (29.43%), Space: 111.3 MB (15.73%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-     int n=nums.size();
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
-        }
-        if(sum%2!=0)
-            return false;
-        else
-        {
-            vector<vector<bool>> dp(n+1,vector<bool>(sum/2+1,false));
-            return func(nums,n,dp,int (sum/2));
-            }
-    }
-    bool func(vector <int> &nums,int n, vector <vector<bool>> &dp,int sum)
+    bool solve(vector<int>& nums,int sum,int n, vector<vector<int>>& dp)
     {
-        
-        for(int i=0;i<n+1;i++)
-        {
-            dp[i][0]=true;
-        }
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<sum+1;j++)
-            {
-                  if(nums[i-1]<=j)
-         dp[i][j]=(dp[i-1][j-nums[i-1]])||(dp[i-1][j]);
-        else
-         dp[i][j]=(dp[i-1][j]);
-            }
-        }
-        
-        
-//         if(sum>0 && n==0)
-//         return false;
-//         if(sum==0)
-//         return true;
-        
-//         if(dp[n][sum]!=false)
-//         return dp[n][sum];
-        
-//         if(nums[n-1]<=sum)
-//         return dp[n][sum]=(func(nums,n-1,dp,sum-nums[n-1])||func(nums,n-1,dp,sum));
-//         else
-//         return dp[n][sum]=func(nums,n-1,dp,sum);
-    return dp[n][sum];
+        if(n==0 && sum!=0)
+        return false;
+        if(sum==0)
+        return true;
+
+        if(dp[n][sum]!=-1)
+        return dp[n][sum];
+
+        //agar current element is gerater than sum
+        if(nums[n-1]>sum)
+        return dp[n][sum] = solve(nums,sum,n-1,dp);
+
+        bool inc = solve(nums,sum-nums[n-1],n-1,dp);
+        bool exc = solve(nums,sum,n-1,dp);
+
+        return dp[n][sum]=(inc || exc);
+    }
+    bool canPartition(vector<int>& nums) {
+      int n = nums.size() ;
+      int sum=0;
+      for(int i=0;i<n;i++)
+      {
+        sum+=nums[i];
+      }
+      if(sum%2!=0)
+      return false;
+      else{ 
+        vector<vector<int>> dp(n+1,vector<int> (sum/2+1,-1));
+        // -1 unassigned 0 false 1 true
+      return solve(nums,sum/2,n,dp); 
+      }
     }
 };
