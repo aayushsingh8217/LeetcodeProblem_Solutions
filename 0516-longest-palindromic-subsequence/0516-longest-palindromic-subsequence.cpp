02afa32cad1dc9c8813d9c7ebe65b81e87d0d4b5
Knight821 <98265835+aayushsingh8217@users.noreply.github.com>
2025-08-17 18:16:08 +0530

    Time: 77 ms (58.3%), Space: 76 MB (21.41%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    int solve(int n, int m,string& a,string& b,vector<vector<int>>& dp)
+    {
+        if(n<=0 || m<=0)
+        return 0;
+
+        if(dp[n][m]!=-1)
+        return dp[n][m];
+
+        if(a[n-1]==b[m-1])
+        return dp[n][m]=1+solve(n-1,m-1,a,b,dp);
+        else
+        return dp[n][m]=max(solve(n-1,m,a,b,dp),solve(n,m-1,a,b,dp));
+    }
     int longestPalindromeSubseq(string s) {
-       string b(s.rbegin(),s.rend());
-        int n=s.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(s[i-1]==b[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n][n];
+        // same as LCS ka variation hai aur kuch nhi
+        // reverse krdo
+        // aur phr dhundo kya LCS hai
+
+        string b(s.rbegin(),s.rend());
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return solve(n,n,s,b,dp);
     }
 };
